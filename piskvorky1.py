6ea16566b125a1ff431eb04c5644151755f45714
Kája <kaja@joja.cz>
2018-04-15 18:45:11 +0200

    Smazána remiza a drobné upravy v tazích hráče.

--- piskvorky1.py
+++ piskvorky1.py
@@ -16,10 +16,8 @@ def tahhrace(m):
 	return tah(m,pozice,"o")
 
 def tahpocitace(m):
-
+	pozice = None
 	for i in range(20):
-
-		pozice = None
 
 		if i<18 and m[i:i+3]=="xx-":
 			pozice = i + 2
@@ -27,48 +25,65 @@ def tahpocitace(m):
 			pozice = i
 		elif i<18 and m[i:i+3]=="x-x":
 			pozice = i + 1
-		elif i<18 and m[i:i+3]=="o-o":
+
+	if pozice is not None:
+		return tah(m, pozice,"x")
+
+	for i in range(20):
+		if i<18 and m[i:i+3]=="o-o":
 			pozice = i + 1
 		elif i<18 and m[i:i+3]=="oo-":
 			pozice = i + 2
 		elif i<18 and m[i:i+3]=="-oo":
 			pozice = i
-		elif i<19 and m[i] == "x" and (m[i+1])== "-":
+
+	if pozice is not None:
+		return tah(m, pozice,"x")
+
+	for i in range(20):
+		if i<18 and m[i:i+3]=="-o-":
+			pozice = i
+	if pozice is not None:
+		return tah(m, pozice,"x")
+
+	for i in range(20):
+		if i<19 and m[i] == "x" and (m[i+1])== "-":
 			pozice = i + 1
 		elif  i>0 and m[i] == "x" and (m[i-1])== "-":
 			pozice = i -1
+	if pozice is not None:
+		return tah(m, pozice,"x")
 
-		else :
-			continue
-
-		return tah(m,pozice,"x")
-
+		
 	while True:
 		pozice = randrange (20)
 		if m[pozice] == "-" :
 			return(tah(m,pozice,"x"))
 
+def poznejviteze ():
+
+	if "xxx" in m:
+		print("Vyhrál počítač")
+		return True
+
+	if "ooo" in m:
+		print("Vyhráli hráč")
+		return True
+
+	if "-" not in m:
+		print("Nikdo nevyhrál")
+		return True
+	return False
+
 
 while True:
 
 	m = tahhrace(m)
-	m = tahpocitace(m)
+	if poznejviteze() :
+		break
 	print(m)
-
-	if ("xxx" in m) and ("ooo" in m):
-		print("Remíza")
+	m = tahpocitace(m)
+	if poznejviteze() :
 		break
 
-	if "xxx" in m:
-		print("Vyhrál počítač")
-		break
-
-	if "ooo" in m:
-		print("Vyhráli hráč")
-		break
-
-	if "-" not in m:
-		print("Nikdo nevyhrál")
-		break
-
-print(m)
+	print(m)
